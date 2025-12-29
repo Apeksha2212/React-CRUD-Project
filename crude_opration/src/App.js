57b2cda32aacadd42517724d9af6f36ac06df716
vsaitech100 <apeksha.gadekar2212@gmail.com>
2025-12-29 14:35:23 +0530

    userform

--- crude_opration/src/App.js
+++ crude_opration/src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import Userform from './componet/forms/Userform';
+
 
 function App() {
   return (
     <div className="App">
       <h1>CRUDE OPRATIONS</h1>
+     <Userform/>
     </div>
   );
 }
